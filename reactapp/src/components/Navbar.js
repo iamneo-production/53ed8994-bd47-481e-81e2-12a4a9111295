529f0e0011e0ecdef11f9ca86ec1c7bc77bf8eb8
aaruwork <arumugham@iamneo.ai>
2022-03-03 15:13:35 +0000

    reactapp completed

---
+++ reactapp/src/components/Navbar.js
@@ -0,0 +1,107 @@
+import styled from 'styled-components';
+import { useStateValue } from "../utils/StateProvider";
+import { actionTypes, openSnackbar } from "../utils/Reducer";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { resetAuthorizationHeader } from '../utils/ApiClient';
+import { useHistory } from 'react-router-dom';
+
+const Container = styled.div`
+  height: 60px;
+  width: 100%;
+  background: #00BFA6;
+  color: #fff;
+  position: fixed;
+  top: 0;
+  z-index: 999999;
+`;
+
+const Wrapper = styled.div`
+  padding: 5px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+function Navbar(){
+
+  const [{userType}, dispatch] = useStateValue();
+  const history = useHistory();
+
+  return (
+    <Container>
+      <Wrapper id="userNavbar">
+        <Left>
+          <Logo>STORE.</Logo>
+          {
+            userType === 'user' ? (
+              <>
+                <MenuItem id='mobileHomeButton' onClick={() => history.push('/home')}>Home</MenuItem>
+                <MenuItem id='mobileCartButton' onClick={() => history.push('/cart')}>Cart</MenuItem>
+                <MenuItem id='mobileOrderButton' onClick={() => history.push('/orders')}>My Orders</MenuItem>
+              </>
+            ) : (
+              <>
+                <MenuItem id='adminProductButton' onClick={() => history.push('/admin')}>Products</MenuItem>
+                <MenuItem id='adminOrderButton' onClick={() => history.push('/admin/orders')}>Orders</MenuItem>
+                <MenuItem id='adminUserButton' onClick={() => history.push('/admin/users')}>Users</MenuItem>
+                <MenuItem id='adminEmailButton' onClick={() => history.push('/admin/email')}>Email</MenuItem>
+              </>
+            )
+          }
+        </Left>
+        <Right>
+          <MenuItem onClick={() => {
+              resetAuthorizationHeader();
+              
+                if(userType === 'user'){
+                  dispatch({
+                    type: actionTypes.SET_USER,
+                    user: null,
+                    userType: null
+                  })
+                  AsyncStorage.clear()
+                  history.push('/')
+                }
+                else{
+                  dispatch({
+                    type: actionTypes.SET_USER,
+                    user: null,
+                    userType: null
+                  })
+                  AsyncStorage.clear()
+                  history.push('/')
+                }
+
+                dispatch(openSnackbar('Thank you for visiting!', 'success'));
+            }} id="logoutButton">Logout</MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+
+export default Navbar;
---
+++ reactapp/src/pages/Signup.js
@@ -0,0 +1,110 @@
+import React , {useState} from 'react'
+import { useStateValue } from "../utils/StateProvider";
+import { openSnackbar } from "../utils/Reducer";
+import { Input, Grid } from '@mui/material'
+import { useHistory } from 'react-router-dom';
+import { ApiClient } from '../utils/ApiClient';
+import LoadingButton from '@mui/lab/LoadingButton';
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function Login() {
+    
+    const [state, dispatch] = useStateValue();
+
+    const [email , setEmail] = useState("")
+    const [username , setUsername] = useState('');
+    const [mobile , setMobile] = useState('')
+    const [password , setPassword] = useState("")
+    const [confirmPassword , setConfirmPassword] = useState("")
+
+    const [emailError , setEmailError] = useState(false);
+    const [usernameError , setusernameError] = useState(false);
+    const [mobError , setMobError] = useState(false);
+    const [passwordError , setPasswordError] = useState(false);
+    const [confirmPasswordError , setConfirmPasswordError] = useState(false);
+    const [loading, setLoading] = useState(false);
+    
+    const history = useHistory();
+
+    const onSubmit = () =>{
+      setEmailError(false);
+      setusernameError(false)
+      setMobError(false)
+      setPasswordError(false)
+      setConfirmPasswordError(false)
+
+      if(!validateEmail(email)){
+        setEmailError(true);
+        dispatch(openSnackbar('Invalid Email.', 'error'));
+        return;
+      }
+
+      if(username.trim() === ''){
+        setusernameError(true);
+        dispatch(openSnackbar('Username cannot be empty.', 'error'));
+        return;
+      }
+
+      if(!(/^\d{10}$/.test(mobile))){
+        setMobError(true);
+        dispatch(openSnackbar('Invalid mobile number.', 'error'));
+        return;
+      }
+
+      if(password.length < 5){
+        setPasswordError(true);
+        dispatch(openSnackbar('Length of password should be at least 5.', 'error'));
+        return;
+      }
+
+      if(password !== confirmPassword){
+        setConfirmPasswordError(true);
+        dispatch(openSnackbar('Confirm password does not match entered password.', 'error'));
+        return;
+      }
+
+      setLoading(true);
+      ApiClient.post('/signup', {
+        email, username, password, mobileNumber: mobile
+      }).then(response => {
+        if (response.data) {
+          // success
+          dispatch(openSnackbar('Welcome to STORE. Please check your inbox for the verification email then login using your new credentials.', 'success'));
+          history.push('/');
+        }
+      }).finally(() => setLoading(false));;
+
+    }
+
+    return (
+      <Grid container maxWidth="md" style={{height : '100vh', justifyContent: 'center', alignItems: 'center', margin: 'auto' }}>
+        <Grid item lg={6} id="signupBox" sx={{ display: 'flex', flexDirection: 'column' }} style={{maxWidth: '20rem'}}>
+          <h1 style = {{marginBottom : '10px'}} >SIGN UP</h1>
+          <Input id = 'email' placeholder = "Enter Email" error = {emailError} type = "email" value = {email} onChange = {(e)=>setEmail(e.target.value)} style ={{margin : '10px'}} />
+          <Input id = 'username' placeholder = "Enter Username" error = {usernameError} type = "text" value = {username} onChange = {(e) => setUsername(e.target.value)} style = {{margin : '10px'}}/>
+          <Input id = 'mobilenumber' placeholder = "Enter Mobile Number" error = {mobError} type = "text" value = {mobile} onChange = {(e) => setMobile(e.target.value)} style = {{margin : '10px'}}/>
+          <Input id = 'password' placeholder = "Enter Password" error = {passwordError} type = "password" value = {password} onChange = {(e) => setPassword(e.target.value)} style = {{margin : '10px'}}/>
+          <Input id = 'confirmpassword' placeholder = "Confirm Password" error = {confirmPasswordError} type = "password" value = {confirmPassword} onChange = {(e) => setConfirmPassword(e.target.value)} style = {{margin : '10px'}}/>
+          <LoadingButton 
+            id = 'submitButton' 
+            variant = 'contained' 
+            onClick = {onSubmit} 
+            style = {{margin : '10px'}} 
+            loading={loading}
+            loadingIndicator="Signing up...">
+            SIGN UP
+          </LoadingButton>
+          <div>Already a member? <a id='signinLink'  href="#" onClick={() => history.push('/') }>Click here</a></div>
+        </Grid>
+        <Grid item lg={6} style={{maxWidth: '20rem'}} sx={{ display: { xs: 'none', md: 'block' } }}>
+          <img src="/images/signup_image.png" />
+        </Grid>
+      </Grid>
+    );
+}
+
+export default Login;
